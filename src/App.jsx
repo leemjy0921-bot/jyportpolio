840a88e38a56db85b60694a6c5cf25251962fd68
jiyoung <leemjy0921@gmail.com>
2026-02-20 10:42:22 +0900

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-blue-500">
+      <h1 className="text-4xl font-bold text-white underline">
+        Tailwind 작동 확인!
+      </h1>
+    </div>
+  )
+}
+
+export default App
